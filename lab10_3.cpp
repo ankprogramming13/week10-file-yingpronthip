4c6c68dd8b17924c8c475dfeafcaaeab1777cab6
Unknown <pronthipza@gmail.com>
2018-01-26 14:15:16 +0700

    ข้อ 3

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,4 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 int main() {
+        int cmd;
+        ofstream writefile ("std.txt", ofstream::app);
+        ifstream readfile ("std.txt");
+        string line;
+        string name;
+
+        cin >> cmd;
+
+        if (cmd == 1){
+            cin >> name;
+            if (writefile.is_open()){
+                writefile << name << endl;
+                writefile.close();
+            }else{
+                cout << "Unable to open file";
+            }
+        }else if (cmd == 2){
+            if (readfile.is_open()){
+                while (getline(readfile,line)){
+                cout << line << endl;
+                }
+                readfile.close();
+            }
+        }else{
+        }
         /* code */
         return 0;
 }
